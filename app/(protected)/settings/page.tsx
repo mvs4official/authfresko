f9470d25befc395c26481d84e162223f88f5cc93
designmvs <dezignmvs@gmail.com>
2024-12-17 20:12:08 +0530

    first commit

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,22 @@
-import { CheckCircledIcon } from "@radix-ui/react-icons"
-import { Button } from "@/components/ui/button"
-import  Link  from "next/link"
-const SettingsPage = () => {
+import React from 'react'
+import { CheckCircledIcon} from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import  Link  from 'next/link';
+
+const PassPage = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-y-6">
-        <div className="bg-emerald-500/15 p-4 rounded-md flex items-center gap-x-2 text-6xl text-emerald-500">
-                <CheckCircledIcon className="h-9 w-9"/>
-                <p>Authenticated</p>
-            </div>
-            <div>
-                <a href="https://auth-homefresko.netlify.app/">
-            <Button className="w-[460px] bg-[#9E896A] hover:bg-[#2a8929c4]">
-               Continue
-            </Button>
-            </a>
-            </div>
+    <div className='h-screen flex flex-col gap-y-4 justify-center items-center'>
+        <div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-2xl text-emerald-500">
+            <CheckCircledIcon className="h-6 w-6"/>
+            <p>Sucess ! You Are Logined</p>
+        </div>
+        <div>
+            <Link href="https://homefresko.netlify.app/">
+            <Button>Continue</Button>
+            </Link>
+        </div>
     </div>
   )
 }
 
-export default SettingsPage
+export default PassPage
